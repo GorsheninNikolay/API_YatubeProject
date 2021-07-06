39634d8507ffa192a0c8022667be666dea1702a5
Fr13ndyy <kolya.gorshenin.2002@gmail.com>
2021-07-06 11:55:43 +0300

    My Project

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 
 from .models import Comment, Follow, Group, Post
+
+# from rest_framework.validators import UniqueTogetherValidator
+
 
 User = get_user_model()
 
